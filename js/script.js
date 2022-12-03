16673c9b86b175cd056dce4a851fccd8f2f1e77f
Pawel Domagala <domagalapawel11@gmail.com>
2022-12-03 23:11:15 +0100

    Add win conditions

--- js/script.js
+++ js/script.js
@@ -1,33 +1,54 @@
+// CPU's move
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
-  computerMove = 'kamień';
+	computerMove = 'kamień';
 }
-else if (playerInput == '2'){
-    computerMove = 'papier';
+else if(randomNumber == 2){
+	computerMove = 'papier';
 }
-else {
-    computerMove = 'nożyce';
+else{
+	computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
-
+// Player's move
 let playerMove = 'nieznany ruch';
+let hint = 'Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-else if (playerInput == '2'){
-    playerMove = 'papier';
-}
-else {
-    playerMove = 'nożyce';
+while(playerMove == 'nieznany ruch'){
+	let playerInput = prompt(hint);
+	console.log('Gracz wpisał: ' + playerInput);
+
+	if(playerInput == '1'){
+		playerMove = 'kamień';
+	}
+	else if(playerInput == '2'){
+		playerMove = 'papier';
+	}
+	else if(playerInput == '3'){
+		playerMove = 'nożyce';
+	}
+	else{
+		hint = 'Błąd: musisz wprowadzić liczbę 1, 2 lub 3.';
+	}
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+// Result
+if(computerMove == playerMove){
+	printMessage('Remis');
+}
+else if((computerMove == 'kamień' && playerMove == 'nożyce') ||
+		(computerMove == 'nożyce' && playerMove == 'papier') ||
+		(computerMove == 'papier' && playerMove == 'kamień')){
+	printMessage('Przegrywasz');
+}
+else{
+	printMessage('Wygrywasz');
+}
